e4e048690a8c7eb1b914e27561d80372b85ad262
ytkasmerti <pikuleva_liza@bk.ru>
2026-04-13 22:39:59 +0500

    геттеры и сеттеры ломбока для WeatherRequest

--- src/main/java/ru/urfu/webapplication/entity/WeatherRequest.java
+++ src/main/java/ru/urfu/webapplication/entity/WeatherRequest.java
@@ -5,11 +5,17 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "weather_requests")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WeatherRequest {
 
     @Id
@@ -25,40 +31,4 @@ public class WeatherRequest {
     private String startDate;
     private String endDate;
     private LocalDateTime requestTime;
-
-    // Конструктор
-    public WeatherRequest() {}
-
-    public WeatherRequest(String city, String requestType, LocalDateTime requestTime) {
-        this.city = city;
-        this.requestType = requestType;
-        this.requestTime = requestTime;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
-
-    public String getRequestType() { return requestType; }
-    public void setRequestType(String requestType) { this.requestType = requestType; }
-
-    public Double getLatitude() { return latitude; }
-    public void setLatitude(Double latitude) { this.latitude = latitude; }
-
-    public Double getLongitude() { return longitude; }
-    public void setLongitude(Double longitude) { this.longitude = longitude; }
-
-    public Integer getForecastDays() { return forecastDays; }
-    public void setForecastDays(Integer forecastDays) { this.forecastDays = forecastDays; }
-
-    public String getStartDate() { return startDate; }
-    public void setStartDate(String startDate) { this.startDate = startDate; }
-
-    public String getEndDate() { return endDate; }
-    public void setEndDate(String endDate) { this.endDate = endDate; }
-
-    public LocalDateTime getRequestTime() { return requestTime; }
-    public void setRequestTime(LocalDateTime requestTime) { this.requestTime = requestTime; }
 }

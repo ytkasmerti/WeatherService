457d786afbb0ce652e4cddd2f5a8068931bd54b9
Daria <dariaabrosimova1980@icloud.com>
2026-04-27 21:45:14 +0500

    создана репозиторий пользователя

---
+++ src/main/java/ru/urfu/webapplication/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package ru.urfu.webapplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.urfu.webapplication.entity.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByApiKey(String apiKey);
+
+    boolean existsByEmail(String email);
+}

d6818357de26bf1ff055cb5825d88a026e5fea4d
Daria <dariaabrosimova1980@icloud.com>
2026-05-25 23:14:54 +0500

    обновлен UserRepository

--- src/main/java/ru/urfu/webapplication/repository/UserRepository.java
+++ src/main/java/ru/urfu/webapplication/repository/UserRepository.java
@@ -14,4 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByApiKey(String apiKey);
 
     boolean existsByEmail(String email);
+
+    List<User> findBySubscriptionExpiresAtBefore(LocalDateTime dateTime);
+
+    List<User> findBySubscriptionExpiresAtBetween(LocalDateTime start, LocalDateTime end);
 }
